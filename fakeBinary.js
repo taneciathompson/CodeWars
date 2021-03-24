6247410f42934a07f9e061ed451a1a1158a4a3fc
taneciathompson <77415322+taneciathompson@users.noreply.github.com>
2021-03-24 09:59:04 -0400

    Create fakeBinary.js

---
+++ fakeBinary.js
@@ -0,0 +1,4 @@
+  
+//Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+let fakeBin = (x) => x.split('').map(num => num >= 5 ? 1 : 0).join('')
